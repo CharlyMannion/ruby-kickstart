d369e455ab431755d07289958f677c29231aceda
Charlotte Mannion <charlottemannion100@gmail.com>
2019-11-21 11:37:10 +0000

    gets user input to work in 2:1

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -3,10 +3,18 @@
 
 # Write a program that reads in two integers typed on the keybaord
 # and outputs their sum, difference, and product
-# 
+#
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
+puts "enter two numbers"
+input_one = gets.chomp
+input_two = gets.chomp
+puts "You said: #{input_one} and #{input_two}"
+
 
 def sum_difference_product
-  # your code goes here
+  input_one = gets
+  input_two = gets
+  puts input_one
+  puts input_two
 end
