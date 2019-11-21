2e7d29414965aca1f5da7dd75c0378640a39c1ce
Charlotte Mannion <charlottemannion100@gmail.com>
2019-11-21 11:39:40 +0000

    puts user input inside the method

--- session2/challenge/1_input_output.rb
+++ session2/challenge/1_input_output.rb
@@ -6,15 +6,17 @@
 #
 # Standard input will be like "9 2\n" and will expect you to print
 # "11\n7\n18\n" to standard output.
-puts "enter two numbers"
-input_one = gets.chomp
-input_two = gets.chomp
-puts "You said: #{input_one} and #{input_two}"
+# puts "enter two numbers"
+# input_one = gets.chomp
+# input_two = gets.chomp
+# puts "You said: #{input_one} and #{input_two}"
 
 
 def sum_difference_product
-  input_one = gets
-  input_two = gets
-  puts input_one
-  puts input_two
+  puts "enter two numbers"
+  input_one = gets.chomp
+  input_two = gets.chomp
+  puts "You said: #{input_one} and #{input_two}"
 end
+
+sum_difference_product
