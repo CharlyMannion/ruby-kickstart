60f68a6c83e48fd9a9e820fabf59833dabe9a86c
Charlotte Mannion <charlottemannion100@gmail.com>
2019-11-22 12:30:08 +0000

    solves 2:4

--- session2/challenge/4_array.rb
+++ session2/challenge/4_array.rb
@@ -7,3 +7,21 @@
 # get_squares [25, 4, 9, 6, 50, 16, 5] # => [4, 5]
 
 # This time you will have to define the method, it's called: get_squares
+
+def get_squares(numbers)
+  numbers.select { |n| numbers.include? n*n }.sort
+end
+
+#   to_return = []
+#   squares_array = numbers.map { |number| number * number }
+#   numbers.each { |number_two|
+#   if squares_array.include?(number_two)
+#     rooted = Math.sqrt(number_two).floor
+#     # puts number_two
+#     to_return.push(rooted)
+#   end
+# }
+# puts to_return
+
+
+# get_squares([9,3,81])
