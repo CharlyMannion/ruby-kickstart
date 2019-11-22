c4206442fde2aa0deae665da35e8e8c6edc9317b
Charlotte Mannion <charlottemannion100@gmail.com>
2019-11-22 18:40:06 +0000

    partial solution to 2:7, need to exclude punctuation

--- session2/challenge/7_array.rb
+++ session2/challenge/7_array.rb
@@ -3,8 +3,16 @@
 # In order to not have to write an actual language parser, there won't be any punctuation too complex.
 # There will be no "'" that is not part of a contraction.
 # Assume each of these  charactsrs are not to be considered: ! @ $ # % ^ & * ( ) - = _ + [ ] : ; , . / < > ? \ |
-# 
+#
 # Examples
 # alternate_words("Lorem ipsum dolor sit amet.")  # => ["Lorem", "dolor", "amet"]
 # alternate_words("Can't we all get along?")      # => ["Can't", "all", "along"]
 # alternate_words("Elementary, my dear Watson!")  # => ["Elementary", "dear"]
+
+def alternate_words(input)
+  words_array = input.scan(/[\w'-]+|\W+/).select {|x| x.match(/\S/)}
+  new_array = words_array.select{|x| words_array.index(x) % 2 == 0}
+  p new_array
+end
+
+alternate_words("Lorem ipsum dolor sit amet.")
